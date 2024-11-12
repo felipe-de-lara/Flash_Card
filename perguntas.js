eb1def94c817fac62bda4c67238762d3da40143c
felipe-de-lara <162244036+felipe-de-lara@users.noreply.github.com>
2024-11-11 22:22:45 -0300

    SALVE

--- perguntas.js
+++ perguntas.js
@@ -5,6 +5,31 @@ criaCartao(
 )
 
 criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+
+
+
+//////////////////////////////////
+
+
+
+criaCartao(
     'Geografia',
     'Qual a capital da França?',
     'A capital da França é Paris'
